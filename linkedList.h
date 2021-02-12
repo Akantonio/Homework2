2559e7de9643dca7cba8326159d579498f2fa150
Akantonio <adrianvantonio@yahoo.com>
2021-02-12 11:01:29 -0900

    added a struct

--- linkedList.h
+++ linkedList.h
@@ -5,4 +5,16 @@
 #ifndef HOMEWORK2_LINKEDLIST_H
 #define HOMEWORK2_LINKEDLIST_H
 
+#include<iostream>
+#include<string>
+
+struct buildingValue{
+    int value;
+    std::string name;
+    double length;
+    double width;
+    double height;
+};
+
+
 #endif //HOMEWORK2_LINKEDLIST_H
--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "catch.hpp"
-
+#include "linkedList.h"
 
 
 
